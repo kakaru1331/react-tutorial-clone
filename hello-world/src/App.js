8a12d914c984b4236a598186c964c581a69c9efa
kakaru <kakaru1331@gmail.com>
2020-08-07 16:59:06 +0900

    till lesson 21. event, props, etc...

---
+++ hello-world/src/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import Ninjas from './Ninjas';
+import AddNinja from './AddNinja';
+
+class App extends Component {
+  state = {
+    ninjas : [
+      { name: 'Ryu', age: 30, belt: 'black' , id: 1 },
+      { name: 'Yoshi', age: 20, belt: 'green' , id: 2 },
+      { name: 'Crystal', age: 25, belt: 'pink' , id: 3 },
+    ]
+  }
+
+  addNinja = (ninja) => {
+    ninja.id = Math.random();
+
+    let ninjas = [...this.state.ninjas, ninja]
+
+    console.log(this.state) 
+    this.setState({
+      ninjas
+    })
+  }
+
+  deleteNinja = (id) => {
+    let ninjas = this.state.ninjas.filter(ninja => {
+      return ninja.id !== id
+    });
+    
+    this.setState({
+      ninjas
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello world!</h1>
+        <p>Welcome :D</p>
+        <Ninjas ninjas={this.state.ninjas} deleteNinja={this.deleteNinja} />
+        <AddNinja addNinja={this.addNinja} />        
+      </div>
+    )
+  }
+}
+
+export default App;
